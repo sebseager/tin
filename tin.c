6f5aa197722e9bfd1dc7ebd2bbf9fd619bd77382
Seager Seb <sjs264@zoo.cs.yale.edu>
2022-09-02 13:55:36 -0400

    Doesn't break lines now

--- tin.c
+++ tin.c
@@ -66,13 +66,9 @@ typedef struct textrow {
 /* prototypes */
 
 int read_key();
+void clear_tty();
 
 /* helpers */
-
-void clear_tty() {
-  write(STDOUT_FILENO, ESC_SEQ "2J", 4); // clear screen
-  write(STDOUT_FILENO, ESC_SEQ "H", 3);  // move cursor to top left
-}
 
 void die(const char *s) {
   clear_tty();
@@ -203,6 +199,11 @@ void init_config() {
 }
 
 /* terminal control */
+
+void clear_tty() {
+  write(STDOUT_FILENO, ESC_SEQ "2J", 4); // clear screen
+  write(STDOUT_FILENO, ESC_SEQ "H", 3);  // move cursor to top left
+}
 
 void disable_raw_tty() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_tty) == -1)
@@ -394,11 +395,60 @@ void draw_rows(abuf *ab) {
         ab_strcat(ab, "~", 1);
       }
     } else {
-      ssize_t len = E.rows[filerow].rlen - E.coloff;
-      if (len < 0)
-        len = 0;
-      else if (len > E.wincols)
-        len = E.wincols;
+      textrow *row = &E.rows[filerow];
+
+      // prevent incomplete utf chars at beginning of line
+      while (UTF_BODY_BYTE(row->render[E.coloff]))
+        E.coloff++;
+
+      // utf chars have multiple bytes, so can show more than E.wincols
+      ssize_t i = E.coloff;
+      ssize_t displen = 0;
+      while (i < row->rlen && displen < E.wincols) {
+        char c = row->render[i++];
+        if (!UTF_BODY_BYTE(c))
+          displen++;
+      }
+
+      // prevent incomplete utf chars at end of line
+      while (UTF_BODY_BYTE(E.rows[filerow].render[i]))
+        i--;
+
+      ssize_t len = i - E.coloff;
+
+      // ssize_t len = row->rlen - E.coloff;
+      // len += row->icounts[0] - row->icounts[E.coloff];
+      // if (len < 0)
+      //   len = 0;
+      // ssize_t width = E.wincols;
+      // width -= row->icounts[E.coloff] - row->icounts[E.coloff + E.wincols];
+
+      // ssize_t end = E.coloff + E.wincols;
+      // if (end >= E.rows[filerow].rlen)
+      //   end = E.rows[filerow].rlen - 1;
+      // ssize_t invislen =
+      //     E.rows[filerow].icounts[E.coloff] - E.rows[filerow].icounts[end];
+      // if (len < 0)
+      //   len = 0;
+      // if (len > E.wincols + invislen)
+      //   len = E.wincols + invislen;
+
+      // ssize_t end = E.coloff + E.wincols;
+      // if (end > E.rows[filerow].rlen)
+      //   end = E.rows[filerow].rlen - 1;
+      // ssize_t invislen =
+      //     E.rows[filerow].icounts[end] - E.rows[filerow].icounts[E.coloff];
+      // ssize_t len = end - E.coloff;
+
+      // // ssize_t len = E.rows[filerow].rlen - E.coloff;
+      // // if (len < 0)
+      // //   len = 0;
+      // // else if (len > E.wincols + invislen)
+      // //   len = E.wincols + invislen;
+
+      FILE *fp = fopen("debug.tmp", "a");
+      fprintf(fp, "len: %zd, coloff: %zd\n", len, E.coloff);
+      fclose(fp);
 
       // draw line number
       char numstr[E.numoff];
@@ -448,13 +498,21 @@ void refresh_screen() {
 /* row logic */
 
 void update_row(textrow *row) {
-  // render tabs as spaces
   ssize_t tabs = 0;
-  for (ssize_t j = 0; j < row->len; j++) {
-    char c = row->chars[j];
-    if (c == TAB_KEY)
+  for (ssize_t i = 0; i >= row->len; i--) {
+    if (row->chars[i] == TAB_KEY)
       tabs++;
   }
+
+  // // keep track of number of invisible chars ahead of each render index
+  // ssize_t icount = 0;
+  // free(row->icounts);
+  // row->icounts = malloc(row->rlen * sizeof(ssize_t));
+  // for (ssize_t i = row->rlen - 1; i >= 0; i--) {
+  //   if (UTF_BODY_BYTE(row->render[i]))
+  //     icount++;
+  //   row->icounts[i] = icount;
+  // }
 
   ssize_t rsize = row->len + tabs * (TIN_TAB_STOP - 1);
   free(row->render);
@@ -462,17 +520,16 @@ void update_row(textrow *row) {
   if (!row->render)
     die("malloc");
 
+  // render tabs as spaces
   ssize_t i = 0;
   for (ssize_t j = 0; j < row->len; j++) {
-    switch (row->chars[j]) {
-    case TAB_KEY:
+    char c = row->chars[j];
+    if (c == TAB_KEY) {
       row->render[i++] = ' ';
       while (i % TIN_TAB_STOP != 0)
         row->render[i++] = ' ';
-      break;
-    default:
+    } else {
       row->render[i++] = row->chars[j];
-      break;
     }
   }
 
@@ -556,9 +613,6 @@ void insert_at_cursor(int c) {
 }
 
 void backspace_at_cursor() {
-  FILE *fp = fopen("debug.tmp", "a");
-  fprintf(fp, "backspace_at_cursor\n");
-  fclose(fp);
   if (E.cx == 0 && E.cy == 0)
     return;
   if (E.cy == E.nrows)
